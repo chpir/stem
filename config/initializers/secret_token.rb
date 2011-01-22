359821f40c6c11f566242a75e07759a17b3794f9
Max Masnick <max@masnick.me>
2011-01-22 13:58:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordFrequenceyRails::Application.config.secret_token = 'f97822d234ea77665648c3d3b77eac152d7b08ed9cf44243bc545269b36075a4c40fcaca50150cca8b95a0629d94b696a40adb5e1526d199d639a2933ddf3819'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WordFrequenceyRails::Application.config.session_store :cookie_store, :key => '_word_frequencey_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# WordFrequenceyRails::Application.config.session_store :active_record_store
